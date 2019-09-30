f1706583e8735682b39a80c01e7b38e78b96accf
maji94 <suparmaji09@gmail.com>
2019-09-30 11:32:27 +0700

    update database

--- database/db_workshop.sql
+++ database/db_workshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2019 at 05:44 AM
+-- Generation Time: Sep 30, 2019 at 06:29 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 5.6.32
 
@@ -56,9 +56,25 @@ CREATE TABLE `tb_pembina` (
 
 CREATE TABLE `tb_peserta` (
   `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `foto` varchar(24) NOT NULL,
   `nip` varchar(32) NOT NULL,
-  `nama` varchar(32) NOT NULL
+  `nama` varchar(32) NOT NULL,
+  `ktp` varchar(19) NOT NULL,
+  `tmp_lahir` varchar(32) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `jns_kelamin` varchar(20) NOT NULL,
+  `agama` varchar(15) NOT NULL,
+  `pendidikan` varchar(15) NOT NULL,
+  `alamat_rm` text NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `nohp` varchar(13) NOT NULL,
+  `golongan` varchar(20) NOT NULL,
+  `jabatan` varchar(20) NOT NULL,
+  `unker` text NOT NULL,
+  `kab` varchar(32) NOT NULL,
+  `alamat_kt` text NOT NULL,
+  `npwp` varchar(32) NOT NULL,
+  `norek` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -69,10 +85,17 @@ CREATE TABLE `tb_peserta` (
 
 CREATE TABLE `tb_user` (
   `id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
+  `username` varchar(24) NOT NULL,
   `password` varchar(32) NOT NULL,
   `hak_akses` enum('admin','pembina','peserta') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `username`, `password`, `hak_akses`) VALUES
+(1, '1771022407940004', '17c4520f6cfd1ab53d8745e84681eb49', 'admin');
 
 -- --------------------------------------------------------
 
@@ -147,7 +170,7 @@ ALTER TABLE `tb_peserta`
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_workshop`
